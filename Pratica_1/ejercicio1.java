8c5f51228f598b2c502cc2bdd8ce567aa3ef4ede
emma-victoriano <c2160394@gmail.com>
2026-08-31 22:59:23 -0500

    Update ejercicio1.java

--- Pratica_1/ejercicio1.java
+++ Pratica_1/ejercicio1.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-
-public class Cuadrado {
-    public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
-        
-        System.out.print("Ingresa la medida de un lado (en cm ): ");
-        int lado=teclado.nextInt();
-        int area=lado*lado;
-        int perimetro=lado*4;
-        
-        System.out.println("El área del cuadrado es: " + area+ "cm²");
-        System.out.println("El perímetro del cuadrado es: " + perimetro+ "cm");
-
-        teclado.close();
+public class Cuadrado
+{
+    public static void main (String args[])
+    {
+        int a,b;
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Este programa calcula el area de un cuadrado");
+        System.out.print("Digita la medida de un lado: ");
+        a = teclado.nextInt();
+        b = a * a;
+        System.out.println("El area del cuadrado es: " +b);
     }
 }
